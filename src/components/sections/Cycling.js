913ee13de36873ac1c8640f1c40bb5e89f04f2b9
ProgramHefe <h.kosonen91@hotmail.com>
2022-09-02 12:41:51 +0300

    Cycling/MTB styled (not final)

--- src/components/sections/Cycling.js
+++ src/components/sections/Cycling.js
@@ -1,35 +1,67 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Bike from '../../images/Bike.jpg'
 import MyBike from '../../images/marlin7.jpg'
 import MyHelmet from '../../images/fox-mainframe.jpg'
+
 import './Cycling.css'
 
 const Cycling = () => {
 
     return (
-        <div className="container">
+        <Container fluid>
             <div className="mtb-introduction">
-                <h1>Mountain Biking</h1>
-                <img src={Bike} alt="Mountain Biking" height="500"></img>
-                <p>
-                    Mountain biking is great way to explore woods and nature and same
-                    time increase your stamina. It is fun way to get to know nature
-                    with speedy descents and atmospheric trails.
-                </p>
+                <Row>
+                    <Col>
+                        <h1>Mountain Biking</h1>
+                        <img 
+                            fluid 
+                            src={Bike} 
+                            alt="Mountain Biking" 
+                            height="500">
+                        </img>
+                        <p>
+                            Mountain biking is great way to explore woods and nature and same
+                            time increase your stamina. It is fun way to get to know nature
+                            with speedy descents and atmospheric trails.
+                        </p>
+                    </Col>
+                </Row>
             </div>
             <div className="my-gear">
-                <img src={MyBike} alt="My own bike" height="500"></img>
-                <p>My bike that i use on trails.</p>
-                <img src={MyHelmet} alt="My own helmet" height="500"></img>
-                <p>My helmet that keeps my head safe.</p>
+                <h3>My Gear</h3>
+                <Row className="my-gear-row">
+                    <Col>
+                        <img fluid src={MyBike} alt="My own bike" height="500"></img>
+                        <p>My bike that i use on trails.</p>
+                    </Col>
+                    <Col>
+                        <img src={MyHelmet} alt="My own helmet" height="500"></img>
+                        <p>My helmet that keeps my head safe.</p>
+                    </Col>
+                </Row>
             </div>
-
-            <iframe 
-            className="mtb-video"
-            title="Mountain biking for beginners"
-            src="https://www.youtube.com/embed/x_Gm4pW-AI8&ab">
-            </iframe>
-        </div>
+            <div>
+                <Row className='mtb-video-row'>
+                    <Col>
+                        <iframe 
+                            className="mtb-video"
+                            title="Mountain biking for beginners"
+                            src="https://www.youtube.com/embed/x_Gm4pW-AI8&ab">
+                        </iframe>
+                    </Col>
+                    <Col>
+                        <p>
+                            This video introduces mountain biking for you
+                            and gives you tips how to start this hobby.
+                        </p>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
     )
 }
 
