09efab243eaf8f21a56944898a10e14e3dc7e100
ProgramHefe <h.kosonen91@hotmail.com>
2022-09-05 11:54:17 +0300

    Tennis and Videogames section made (not final)

--- src/components/sections/Tennis.js
+++ src/components/sections/Tennis.js
@@ -1,44 +1,72 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Tenniss from '../../images/tennis.jpg'
 import Racquet from '../../images/Tennis_Racquet.jpg'
 import Shoes from '../../images/tennis_shoes.jpg'
+
 import './Tennis.css'
 
 
 const Tennis = () => {
 
     return (
-        <div className="container">
+        <Container fluid>
             <div className="tennis-introduction">
-                <h1>Tennis</h1>
-                <img src={Tenniss} alt="tennis"></img>
-                <p>
-                    Tennis is very nice way to get your competitive side pleasured.
-                    It is nice way to get your hand-eye cordination better and stamina wise
-                    too. You can play it 1v1 on 2v2.
-                </p>
+                <Row>
+                    <Col className="head-column">
+                        <h1>Tennis</h1>
+                    </Col>
+                </Row>
+                <Row className="tennis-intro-row">
+                    <Col xs={12} sm={4} md={4}>
+                        <img fluid src={Tenniss} alt="tennis"></img>
+                    </Col>
+                    <Col xs={12} sm={4} md={4}>
+                        <p className="tennis-explanation">
+                            Tennis is very nice way to get your competitive side pleasured.
+                            It is nice way to get your hand-eye cordination better and stamina wise
+                            too. You can play it 1v1 on 2v2.
+                        </p>
+                    </Col>
+                </Row>
             </div>
             <div className="tennis-gear">
-                <img src={Racquet} alt="Tennis racquet"></img>
-                <p>
-                    Tennis racquet is essential for this hobby. There is
-                    many different type of racquets and i recommend to start
-                    with beginner friendly version.
-                </p>
-                <img src={Shoes} alt="Tennis shoes"></img>
-                <p>
-                    Tennis shoes are not that important but they give you edge 
-                    with better grip and suitability to court. Running shoes
-                    are also valid for playing tennis.
-                </p>
-                <iframe 
-                className="tennis-video"
-                src="https://www.youtube.com/embed/YqgcykDGB2A&ab">
+                <Row className="tennis-gear-row">
+                    <Col >
+                        <h3>
+                            Tennis Gear
+                        </h3>
+                    </Col>
+                    <Col>
+                        <img fluid src={Racquet} alt="Tennis racquet"></img>
+                        <p>
+                            Tennis racquet is essential for this hobby. There is
+                            many different type of racquets and i recommend to start
+                            with beginner friendly version.
+                        </p>
+                    </Col>
+                    <Col>
+                        <img fluid src={Shoes} alt="Tennis shoes"></img>
+                        <p>
+                            Tennis shoes are not that important but they give you edge 
+                            with better grip and suitability to court. Running shoes
+                            are also valid for playing tennis.
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="tennis-video-row">
+                    <iframe 
+                        className="tennis-video"
+                        src="https://www.youtube.com/embed/YqgcykDGB2A&ab">
                 
-                </iframe>
-                <p>Little video for you to get to know each type of shot.</p>
+                        </iframe>
+                    <p>Little video for you to get to know each type of shot.</p>
+                </Row>
             </div>
-        </div>
+        </Container>
     )
 }
 
--- src/components/sections/VideoGames.js
+++ src/components/sections/VideoGames.js
@@ -1,30 +1,68 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Games from '../../images/video-games.jpg'
 import CSGO from '../../images/csgo.jpg'
 import Uncharted from '../../images/uncharted.jpg'
+
 import './VideoGames.css'
+
 
 
 const VideoGames = () => {
 
     return (
-        <div className="container">
+        <Container fluid>
             <div className="introduction">
-                <h1>Video Games</h1>
-                <img src={Games} alt="Video Games"></img>
-                <p>
-                    Video games are good way to kill time and hang out with your friends online.
-                    Gaming could be relaxing and challenging social or both at the same time.
-                    You can make new friends in onloine games or explore wonderful stories
-                    which you could not imagine.
-                </p>
+                <Row>
+                    <h1>Video Games</h1>
+                </Row>
+                <Row className='games-intro-row'>
+                    <Col>
+                        <img fluid src={Games} alt="Video Games"></img>
+                    </Col>
+                    <Col>
+                        <p>
+                            Video games are good way to kill time and hang out with your friends online.
+                            Gaming could be relaxing and challenging social or both at the same time.
+                            You can make new friends in onloine games or explore wonderful stories
+                            which you could not imagine.
+                        </p>
+                    </Col>
+                </Row>
             </div>
             <div className="favourite-games">
-                <h3>Favourite Games</h3>
-                <img src={CSGO} alt="Counter Strike"></img>
-                <img src={Uncharted} alt="Uncharted game"></img>
+                <Row>
+                    <h3>Favourite Games</h3>
+                </Row>
+                <Row>
+                    <Col>
+                        <img fluid src={CSGO} alt="Counter Strike"></img>
+                    </Col>
+                    <Col>
+                        <p>
+                            Counter-Strike is a tactical shooter and it is based 
+                            much on cummunications and temwork. It is best played
+                            with friends and that is why i still play it regularly.
+                         </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img src={Uncharted} alt="Uncharted game"></img>
+                    </Col>
+                    <Col>
+                        <p>
+                            Uncharted is action adventure game series that serves
+                            you one of the best action scenes in gaming and memorable characters
+                            and stories. Most memorable and fun story based game series for me.
+                        </p>
+                    </Col>
+                </Row>
             </div>
-        </div>
+        </Container>
     )
 }
 
